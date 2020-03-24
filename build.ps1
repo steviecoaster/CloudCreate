6ba0d1ae9c3a259570107e5e8650b908ff2f439c
Stephen Valdinger <stephen@chocolatey.io>
2020-03-24 10:34:05 -0400

    Build script errors again.

--- build.ps1
+++ build.ps1
@@ -1,16 +1,3 @@
-<#
-Set-ExecutionPolicy Bypass -Scope Process -Force
-
-@('https://raw.githubusercontent.com/steviecoaster/CloudCreate/master/Build/InstallChocolatey.ps1',
-'https://raw.githubusercontent.com/steviecoaster/CloudCreate/master/Build/InstallSelenium.ps1',
-'https://raw.githubusercontent.com/steviecoaster/CloudCreate/master/Build/InstallNexusAndSeed.ps1',
-'https://raw.githubusercontent.com/steviecoaster/CloudCreate/master/Build/InstallJenkins.ps1',
-'https://raw.githubusercontent.com/steviecoaster/CloudCreate/master/Build/FirewallRules.ps1') | Foreach-Object {
-
-    Invoke-Expression (New-Object System.Net.WebClient).DownloadString("$_")
-}
-#>
-
 #region Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1')
