11aa8157464afcd892f0a336c367f62d729d9319
Stephen Valdinger <stephen@chocolatey.io>
2020-03-25 12:35:09 -0400

    Param test passed. Trying schTask now

--- build.ps1
+++ build.ps1
@@ -2,11 +2,11 @@
 param(
   [parameter()]
   [string]
-  $Test1,
+  $TaskUser,
 
   [parameter()]
   [string]
-  $Test2
+  $TaskPass
 )
 
 Function Install-Chocolatey {
@@ -238,160 +238,181 @@ Function Set-NexusFirewall {
 }
 
 Function Install-Jenkins {
-  choco install jenkins -y --no-progress -s https://chocolatey.org/api/v2 --no-progress
-  choco install googlechrome --version=79.0.3945.130 -s https://chocolatey.org/api/v2 -y --ignore-checksums --no-progress
-  choco install selenium-chrome-driver --version=79.0.3945.36 -y --no-progress -s https://chocolatey.org/api/v2
-  $VerbosePreference = 'Continue'
-  Import-Module Selenium -Force -Verbose
 
-  Write-Verbose -Message "Ensuring appropriate Chrome Assembly is loaded"
-  $ModuleBase = (Get-Module -ListAvailable Selenium).ModuleBase
-  Unblock-File -Path 'C:\tools\selenium\*'
-  Get-Process -Name *chrome*, *firefox*, *gecko* | Stop-Process -Force
-  Get-ChildItem -Path 'C:\tools\selenium\*' | Copy-Item -Destination "$ModuleBase\assemblies\" -Force -ErrorAction SilentlyContinue
+  process {
+    choco install jenkins -y --no-progress -s https://chocolatey.org/api/v2 --no-progress
+    choco install googlechrome --version=79.0.3945.130 -s https://chocolatey.org/api/v2 -y --ignore-checksums --no-progress
+    choco install selenium-chrome-driver --version=79.0.3945.36 -y --no-progress -s https://chocolatey.org/api/v2
+    $VerbosePreference = 'Continue'
+    Import-Module Selenium -Force -Verbose
 
-  # Start Jenkins in Selenium
+    Write-Verbose -Message "Ensuring appropriate Chrome Assembly is loaded"
+    $ModuleBase = (Get-Module -ListAvailable Selenium).ModuleBase
+    Unblock-File -Path 'C:\tools\selenium\*'
+    Get-Process -Name *chrome*, *firefox*, *gecko* | Stop-Process -Force
+    Get-ChildItem -Path 'C:\tools\selenium\*' | Copy-Item -Destination "$ModuleBase\assemblies\" -Force -ErrorAction SilentlyContinue
 
-  $driver = Start-SeChrome -Maximized -Headless
-  # Enforce a reasonable window size
-  $driver.Manage().Window.Size = [System.Drawing.Size]::new(1600, 900)
-  Start-Sleep -Seconds 20
+    # Start Jenkins in Selenium
 
-  Enter-SeUrl -Url http://localhost:8080 -Driver $driver
-  Start-Sleep -Seconds 3
+    $driver = Start-SeChrome -Maximized -Headless
+    # Enforce a reasonable window size
+    $driver.Manage().Window.Size = [System.Drawing.Size]::new(1600, 900)
+    Start-Sleep -Seconds 20
 
-  Write-Verbose -Message "Loaded Jenkins UI"
+    Enter-SeUrl -Url http://localhost:8080 -Driver $driver
+    Start-Sleep -Seconds 3
 
-  # Login to UI
-  gc 'C:\Program Files (x86)\Jenkins\secrets\initialAdminPassword' | Set-Clipboard
-  $input = Find-SeElement -Driver $driver -Id 'security-token'
-  Send-SeKeys -Element $input -Keys "$(Get-Clipboard)"
-  Start-Sleep -Seconds 5
-  Write-Verbos -Message "Logged into UI"
+    Write-Verbose -Message "Loaded Jenkins UI"
 
-  $Continue = Find-SeElement -Driver $driver -XPath '/html/body/div[2]/div/form/div[1]/div/div/div/div[3]/input'
-  Invoke-SeClick -Driver $driver -Element $Continue
-  Start-Sleep -Seconds 5
+    # Login to UI
+    gc 'C:\Program Files (x86)\Jenkins\secrets\initialAdminPassword' | Set-Clipboard
+    $input = Find-SeElement -Driver $driver -Id 'security-token'
+    Send-SeKeys -Element $input -Keys "$(Get-Clipboard)"
+    Start-Sleep -Seconds 5
+    Write-Verbos -Message "Logged into UI"
 
-  Write-Verbose -Message "Installing Suggested Plugins"
+    $Continue = Find-SeElement -Driver $driver -XPath '/html/body/div[2]/div/form/div[1]/div/div/div/div[3]/input'
+    Invoke-SeClick -Driver $driver -Element $Continue
+    Start-Sleep -Seconds 5
 
-  # Install Suggested Plugins
-  $SuggestedPlugins = Find-SeElement -Driver $driver -XPath '/html/body/div[2]/div/div/div/div/div/div/div[2]/div/p[2]/a[1]'
-  Invoke-SeClick -Driver $driver -Element $SuggestedPlugins
-  Start-Sleep -Seconds 240
+    Write-Verbose -Message "Installing Suggested Plugins"
 
-  Write-Verbose -Message "Installed suggested Plugins"
+    # Install Suggested Plugins
+    $SuggestedPlugins = Find-SeElement -Driver $driver -XPath '/html/body/div[2]/div/div/div/div/div/div/div[2]/div/p[2]/a[1]'
+    Invoke-SeClick -Driver $driver -Element $SuggestedPlugins
+    Start-Sleep -Seconds 240
 
-  #continue as admin
-  $ContinueAsAdmin = Find-SeElement -Driver $driver -XPath '/html/body/div[2]/div/div/div/div/div/div/div[3]/button[1]'
-  Invoke-SeClick -Element $ContinueAsAdmin -Driver $driver
-  Start-Sleep -Seconds 3
+    Write-Verbose -Message "Installed suggested Plugins"
 
-  #Click Save and Finish Button
-  $Save = Find-SeElement -Driver $driver -XPath '/html/body/div[2]/div/div/div/div/div/div/div[3]/button[2]'
-  Invoke-SeClick -Element $Save  -Driver $driver
-  Start-Sleep -Seconds 3
+    #continue as admin
+    $ContinueAsAdmin = Find-SeElement -Driver $driver -XPath '/html/body/div[2]/div/div/div/div/div/div/div[3]/button[1]'
+    Invoke-SeClick -Element $ContinueAsAdmin -Driver $driver
+    Start-Sleep -Seconds 3
 
-  #Start using Jenkins
-  $Start = Find-SeElement -Driver $driver -XPath '/html/body/div[2]/div/div/div/div/div/div/div[2]/div/button'
-  Invoke-SeClick -Element $Start -Driver $driver
-  Start-Sleep -Seconds 3
+    #Click Save and Finish Button
+    $Save = Find-SeElement -Driver $driver -XPath '/html/body/div[2]/div/div/div/div/div/div/div[3]/button[2]'
+    Invoke-SeClick -Element $Save  -Driver $driver
+    Start-Sleep -Seconds 3
 
-  Write-Verbose "Disabling security to import Pipeline jobs"
-  #Manage Jenkins 
-  $ManageJenkins = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[1]/div[1]/div[4]/a[2]'
-  Invoke-SeClick -Element $ManageJenkins -Driver $driver
-  Start-Sleep -Seconds 3
+    #Start using Jenkins
+    $Start = Find-SeElement -Driver $driver -XPath '/html/body/div[2]/div/div/div/div/div/div/div[2]/div/button'
+    Invoke-SeClick -Element $Start -Driver $driver
+    Start-Sleep -Seconds 3
 
-  #Global Security
-  $GlobalSecurity = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[2]/div[3]/a'
-  Invoke-SeClick -Element $GlobalSecurity -Driver $driver
-  Start-Sleep -Seconds 3
+    Write-Verbose "Disabling security to import Pipeline jobs"
+    #Manage Jenkins 
+    $ManageJenkins = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[1]/div[1]/div[4]/a[2]'
+    Invoke-SeClick -Element $ManageJenkins -Driver $driver
+    Start-Sleep -Seconds 3
 
-  #Implement Terrible security practices
-  $Security = Find-SeElement -Driver $driver -XPath '/html/body/div[6]/div[2]/form/table/tbody/tr[6]/td[3]/table/tbody/tr[25]/td[1]/label/input'
-  Invoke-SeClick -Element $Security -Driver $driver
-  Start-Sleep -Milliseconds 100
+    #Global Security
+    $GlobalSecurity = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[2]/div[3]/a'
+    Invoke-SeClick -Element $GlobalSecurity -Driver $driver
+    Start-Sleep -Seconds 3
 
-  #Save Changes
-  $SaveChanges = Find-SeElement -Driver $driver -XPath '/html/body/div[6]/div[2]/form/table/tbody/tr[60]/td/div[2]/div[2]/span[1]/span/button'
-  Invoke-SeClick -Element $SaveChanges -Driver $driver
-  Start-Sleep -Seconds 3
+    #Implement Terrible security practices
+    $Security = Find-SeElement -Driver $driver -XPath '/html/body/div[6]/div[2]/form/table/tbody/tr[6]/td[3]/table/tbody/tr[25]/td[1]/label/input'
+    Invoke-SeClick -Element $Security -Driver $driver
+    Start-Sleep -Milliseconds 100
 
-  Write-Verbose -Message "Importing Pipeline jobs"
-  $xml = Get-ChildItem C:\jenkinsxml -Filter *.xml
-  $xml | % { Get-Content $_.Fullname | &'C:\Program Files (x86)\Jenkins\jre\bin\java.exe' -jar C:\jenkinsxml\jenkins-cli.jar -s http://localhost:8080/ create-job "$($_.basename)" }
-  Remove-Item C:\jenkinsxml -Recurse -Force
+    #Save Changes
+    $SaveChanges = Find-SeElement -Driver $driver -XPath '/html/body/div[6]/div[2]/form/table/tbody/tr[60]/td/div[2]/div[2]/span[1]/span/button'
+    Invoke-SeClick -Element $SaveChanges -Driver $driver
+    Start-Sleep -Seconds 3
 
-  Write-Verbose -Message "Enabling hardened security"
-  #Manage Jenkins 
-  $ManageJenkins = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[1]/div[1]/div[4]/a[2]'
-  Invoke-SeClick -Element $ManageJenkins -Driver $driver
-  Start-Sleep -Seconds 3
+    Write-Verbose -Message "Importing Pipeline jobs"
+    $xml = Get-ChildItem C:\jenkinsxml -Filter *.xml
+    $xml | % { Get-Content $_.Fullname | &'C:\Program Files (x86)\Jenkins\jre\bin\java.exe' -jar C:\jenkinsxml\jenkins-cli.jar -s http://localhost:8080/ create-job "$($_.basename)" }
+    Remove-Item C:\jenkinsxml -Recurse -Force
 
-  #Global Security
-  $GlobalSecurity = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[2]/div[3]/a'
-  Invoke-SeClick -Element $GlobalSecurity -Driver $driver
-  Start-Sleep -Seconds 3
+    Write-Verbose -Message "Enabling hardened security"
+    #Manage Jenkins 
+    $ManageJenkins = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[1]/div[1]/div[4]/a[2]'
+    Invoke-SeClick -Element $ManageJenkins -Driver $driver
+    Start-Sleep -Seconds 3
 
-  #Fix Terrible security practices
-  $Security = Find-SeElement -Driver $driver -XPath '/html/body/div[6]/div[2]/form/table/tbody/tr[6]/td[3]/table/tbody/tr[33]/td[1]/label/input'
-  Invoke-SeClick -Element $Security -Driver $driver
-  Start-Sleep -Milliseconds 100
+    #Global Security
+    $GlobalSecurity = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[2]/div[3]/a'
+    Invoke-SeClick -Element $GlobalSecurity -Driver $driver
+    Start-Sleep -Seconds 3
 
-  #Save Changes
-  $SaveChanges = Find-SeElement -Driver $driver -XPath '/html/body/div[6]/div[2]/form/table/tbody/tr[60]/td/div[2]/div[2]/span[1]/span/button'
-  Invoke-SeClick -Element $SaveChanges -Driver $driver
-  Start-Sleep -Seconds 5
+    #Fix Terrible security practices
+    $Security = Find-SeElement -Driver $driver -XPath '/html/body/div[6]/div[2]/form/table/tbody/tr[6]/td[3]/table/tbody/tr[33]/td[1]/label/input'
+    Invoke-SeClick -Element $Security -Driver $driver
+    Start-Sleep -Milliseconds 100
 
-  Write-Verbose -Message "Enabling PowerShell Plug-in"
-  Write-Verbose "Clicking Manage jenkins"
-  $ManageJenkins = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[1]/div[1]/div[4]/a[2]'
-  Invoke-SeClick -Element $ManageJenkins -Driver $driver
-  Start-Sleep -Seconds 3
+    #Save Changes
+    $SaveChanges = Find-SeElement -Driver $driver -XPath '/html/body/div[6]/div[2]/form/table/tbody/tr[60]/td/div[2]/div[2]/span[1]/span/button'
+    Invoke-SeClick -Element $SaveChanges -Driver $driver
+    Start-Sleep -Seconds 5
 
-  # Manage Plugins
-  Write-Verbose "Clicking Manage Plugins"
-  $ManagePlugins = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[2]/div[7]/a'
-  Invoke-SeClick -Element $ManagePlugins -Driver $driver
-  Start-Sleep -Seconds 3
+    Write-Verbose -Message "Enabling PowerShell Plug-in"
+    Write-Verbose "Clicking Manage jenkins"
+    $ManageJenkins = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[1]/div[1]/div[4]/a[2]'
+    Invoke-SeClick -Element $ManageJenkins -Driver $driver
+    Start-Sleep -Seconds 3
 
-  # Click Available tab
-  Write-Verbose "Clicking Available Tab"
-  $Available = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[2]/form/div[1]/div[1]/div[2]/a'
-  Invoke-SeClick -Element $Available -Driver $driver
-  Start-Sleep -Seconds 3
+    # Manage Plugins
+    Write-Verbose "Clicking Manage Plugins"
+    $ManagePlugins = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[2]/div[7]/a'
+    Invoke-SeClick -Element $ManagePlugins -Driver $driver
+    Start-Sleep -Seconds 3
 
-  # Filter for PowerShell
-  Write-Verbose "Filtering for PowerShell"
-  $filterBox = Find-SeElement -Driver $driver -Id 'filter-box'
-  Send-SeKeys -Element $filterBox -Keys 'Powershell'
+    # Click Available tab
+    Write-Verbose "Clicking Available Tab"
+    $Available = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[2]/form/div[1]/div[1]/div[2]/a'
+    Invoke-SeClick -Element $Available -Driver $driver
+    Start-Sleep -Seconds 3
 
-  #Tick check box
-  Write-Verbose "Selecting PowerShell"
-  $TickBox = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[2]/form/div[2]/table/tbody/tr[11]/td[1]/input'
-  Invoke-SeClick -Element $TickBox -Driver $driver
+    # Filter for PowerShell
+    Write-Verbose "Filtering for PowerShell"
+    $filterBox = Find-SeElement -Driver $driver -Id 'filter-box'
+    Send-SeKeys -Element $filterBox -Keys 'Powershell'
 
-  # Install w/o Restart
-  Write-Verbose "Clicking Install w/o Restart"
-  $InstallButton = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[2]/form/div[4]/div[2]/span[1]/span/button'
-  Invoke-SeClick -Element $InstallButton -Driver $driver
-  Start-Sleep -Seconds 15
+    #Tick check box
+    Write-Verbose "Selecting PowerShell"
+    $TickBox = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[2]/form/div[2]/table/tbody/tr[11]/td[1]/input'
+    Invoke-SeClick -Element $TickBox -Driver $driver
 
-  Write-Verbose -Message 'Closing down Selenium, we are all set!'
-  Stop-SeDriver -Driver $driver
+    # Install w/o Restart
+    Write-Verbose "Clicking Install w/o Restart"
+    $InstallButton = Find-SeElement -Driver $driver -XPath '/html/body/div[4]/div[2]/form/div[4]/div[2]/span[1]/span/button'
+    Invoke-SeClick -Element $InstallButton -Driver $driver
+    Start-Sleep -Seconds 15
+
+    Write-Verbose -Message 'Closing down Selenium, we are all set!'
+    Stop-SeDriver -Driver $driver
+
+  }
+  
 }
 
 Function New-JenkinsScheduledTask {
+  [cmdletbinding()]
+  param(
+    [parameter()]
+    [string]
+    $User,
 
+    [parameter()]
+    [string]
+    $Pass
+  )
   process {
+
+    ${Function:\Install-Jenkins} | Set-Content "C:\scripts\Jenkins.ps1"
     $action = New-ScheduledTaskAction -Execute 'C:\windows\system32\WindowsPowerShellv1.0\powershell.exe' -Argument "-NonInteractive -Nologo -NoProfile -File 'C:\scripts\Jenkins.ps1'" -WorkingDirectory 'C:\scripts'
     $trigger = New-ScheduledTaskTrigger -Once -At 3am
     $settings = New-ScheduledTaskSettingsSet   -DontStopOnIdleEnd -Restart-Interval (New-Timespan -Minutes 1) -RestartCount 10 -StartWhenAvailable
     $settings.ExecutionTimeout = "PT0S"
 
-    $cred =
-    Install-Jenkins
+    $cred = New-object System.Management.Automation.PSCredential -ArgumentList $TaskUser, "$(ConvertTo-SecureString $Pass -AsPlainText -Force)"
+
+    $Task = New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Settings
+
+    $Task | Register-ScheduledTask -TaskName  'JenkinsSetup' -User $TaskUser -Password "$($cred.GetNetworkCredential().Password)"
+
+    #Start-ScheduledTask -TaskName 'Jenkins Setup'
 
   }
 }
@@ -402,7 +423,7 @@ Function New-JenkinsScheduledTask {
 #Get-Scripts
 #Install-Nexus
 #Set-NexusFirewall
-#New-JenkinsScheduledTask
+New-JenkinsScheduledTask -User $TaskUser -Pass $TaskPass
 
 
 $InformationString = @"
@@ -419,7 +440,3 @@ Password: $(Get-Content "C:\Program Files (x86)\jenkins\secrets\initialAdminPass
 "@
 
 #Write-Host $InformationString
-
-
-Write-Host "You passed: $Test1"
-Write-Host "You also passed: $Test2"
